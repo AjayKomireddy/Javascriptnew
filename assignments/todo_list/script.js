dcefedccf466800460fd9c87beca57c5b66b8d04
Ajay Komireddy <ajay.komireddy658@gmail.com>
2021-12-13 20:21:24 +0530

    apicommit 1

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,41 +1,49 @@
-// var maindiv=document.querySelector('div')
-// var input=maindiv.children[0]
-// var button=maindiv.children[1]
-// var divtodos=document.querySelector('.todos')
-// button.addEventListener("click" ,displayInput);
+
+// let inputValue=document.getElementsByTagName('input')[0];
+// let Addtobutton=document.getElementsByTagName('button')[0];
+// let divtodos=document.getElementsByClassName('todos')[0];
+// var x=0;
+// Addtobutton.onclick=displayInput;
 
 // function displayInput(){
-
-//    var x = input.value;
-//    var p=document.createElement('p');
-//    p.innerText=x;
-//    divtodos.appendChild('p');
-
+//     if(inputValue.value == "" || inputValue.value==0){
+//         alert("invalid input")
+//     }
+//     else {
+//         var insidetag=document.createElement('p');//<p key=0  >"Ajay"</p>
+//         var attri=document.createAttribute('key');
+//         var insidediv1=document.createElement('div');
+//         attri.value=x;                        //key=0
+//         insidetag.setAttributeNode(attri);
+//         insidetag.innerHTML=inputValue.value;
+//         divtodos.appendChild(insidetag);
+//         inputValue.value="";
+//         x++;
+//         insidetag.onclick=function () { 
+//             divtodos.removeChild(insidetag);
+//         }
+//     }
 // }
 
-let inputValue=document.getElementsByTagName('input')[0];
-let Addtobutton=document.getElementsByTagName('button')[0];
-let divtodos=document.getElementsByClassName('todos')[0];
+// Todo Assignment from API file.
 
-var x=0;
-Addtobutton.onclick=displayInput;
-function displayInput(){
-    if(inputValue.value == "" || inputValue.value==0){
-        alert("invalid input")
+// let btnpending=document.querySelector('.Pending')
+// let btncompleted=document.querySelector('.Completed')
+var todos=document.querySelector('div.todos')
+async function main(){
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const json = await response.json();
+        const todos = json.slice(0,10);
+        console.log(todos);
+    }catch(e){
+        console.log(e);
     }
-    else {
-        var insidetag=document.createElement('p');//<p key=0  >"Ajay"</p>
-        var attri=document.createAttribute('key');
-        attri.value=x;                        //key=0
-        insidetag.setAttributeNode(attri);
-        insidetag.innerHTML=inputValue.value;
-        divtodos.appendChild(insidetag);
-        inputValue.value="";
-        x++;
-        insidetag.onclick=function () { 
-            divtodos.removeChild(insidetag);
-        }
-    }
+
 }
+main()
+var insideTag=document.createElement('p');
+insideTag.innerHTML=todos;
+todos.appendChild(insideTag);
 
 
